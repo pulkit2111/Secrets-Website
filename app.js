45b4f4b665e584c60a2898b34149fd3c2b20cd07
Pulkit Mangla <pulkitmangla.2111@gmail.com>
2023-12-25 14:41:38 +0530

    sessions and passport and cookies executed successfully

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 //jshint esversion:6
-require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.json());
+// app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+//session 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1/secrets');
@@ -22,15 +33,22 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-console.log(process.env.SECRET);
 
 // user schema
 const userSchema = new mongoose.Schema({
-    email: String,
+    username: String,
     password: String
 } ,{ versionKey: false });
 
+//passport-local-mongoose
+userSchema.plugin(passportLocalMongoose);
+
 const userDetails = new mongoose.model('registrations', userSchema);
+
+passport.use(userDetails.createStrategy());
+
+passport.serializeUser(userDetails.serializeUser());
+passport.deserializeUser(userDetails.deserializeUser());
 
 app.get('/', function(req, res){
     res.render("home")
@@ -46,61 +64,52 @@ app.get('/register', function(req, res){
     res.render("register")
 })
 
-app.post('/register', async(req,res) => {
-    try {
-        // const userData = req.body;
-        const newUser = new userDetails({
-            email: req.body.email,
-            password: md5(req.body.password)
-        });
-        await newUser.save();
+app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  });
+
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
         res.render("secrets");
-        // res.status(201).json({message: 'Registered successfully!'});
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-      }
+    }
+    else{
+        res.redirect("/login");
+    }
 })
 
-app.post('/login', async(req, res) => {
-    const email= req.body.email;
-    const password = md5(req.body.password);
-
+app.post('/register', async (req, res) => {
     try {
-        const user = await userDetails.findOne({ 'email': email });
-
-        if (!user) {
-            return res.status(401).send("Invalid Username And Password.");
-        }
-
-        // Compare the encrypted form of the entered password with the stored encrypted password
-        if (user.password === password) {
-            // Passwords match, user is authenticated
-            res.render("secrets");
-        } else {
-            // Passwords do not match
-            res.status(401).send("Invalid Username And Password.");
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        const newUser = new userDetails({ username: req.body.username });
+        await userDetails.register(newUser, req.body.password);
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/secrets");
+        });
+    } catch (err) {
+        console.error(err);
+        // Display a specific error message on the registration page
+        res.render("register", { registrationError: "Registration failed. Please try again." });
     }
 });
 
-// app.post('/login', async(req,res) =>{
-//     const{email, password} = req.body;
-
-//     const existingRecord = await userDetails.findOne({
-//         'email': email,
-//        'password' : password 
-//     });
-
-//     if(!existingRecord){
-//         return res.status(401).send("Invalid Username And Password.")
-//     }
-//     res.render("secrets");
-//     //res.status(200).send('You have successfully logged in.');
-// })
+app.post('/login', async(req, res) => {
+    const user = new userDetails({
+        username: req.body.username,
+        password: req.body.password
+    }) 
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
+});
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000.")
